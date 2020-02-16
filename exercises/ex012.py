3b1d2f78d4d46fbf30c9d4ae10ba2e48efe57df5
Marcos V. Leal <marcosv.lealferreira@gmail.com>
2020-02-16 19:46:12 -0300

    Primeiro for

---
+++ exercises/ex012.py
@@ -0,0 +1,12 @@
+# Estrutura de Repeticao
+
+for c in range(0, 5):
+    print('CRESCENTE ', c)
+
+for x in range(5, 0, -1):
+    print('DECRESCENTE ', x)
+
+for y in range(0, 5, 2):
+    print('CRESCENTE 2x2 ', y)
+
+print('--FIM--')
