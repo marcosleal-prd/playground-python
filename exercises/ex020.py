da758225a6867980938b6dc5f7ec5fbe6dd5c17e
Marcos V. Leal <marcosv.lealferreira@gmail.com>
2020-02-19 22:26:35 -0300

    Tratamento de erros e excecoes

---
+++ exercises/ex020.py
@@ -0,0 +1,17 @@
+# Tratamento de Erros e Execeçoes
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Algo errado com os tipos informados :(')
+except ZeroDivisionError:
+    print('Não é possível dividir por ZERO :(')
+except KeyboardInterrupt:
+    print('Usuário preferiu desistir :(')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__cause__}')
+else:
+    print(f'Deu certo, o resultado é: {r}')
+finally:
+    print('\nVolte sempre ;)')
