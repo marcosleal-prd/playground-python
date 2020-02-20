822b79c35f425a51106925fe8d255782b1150967
Marcos V. Leal <marcosv.lealferreira@gmail.com>
2020-02-19 22:38:13 -0300

    Adiciona projeto HealthCheck básico

---
+++ projects/health-check/main.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://pudim.com.br')
+except urllib.error.URLError:
+    print('Site não acessível no momento :(')
+except Exception as error:
+    print(f'Algo errado -> {error.__cause__}')
+else:
+    print('Tudo certo com o site :)')
