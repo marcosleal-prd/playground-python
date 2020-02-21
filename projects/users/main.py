e1c8a88f95c93bf8cca19073cd8cebdaddb31134
Marcos V. Leal <marcosv.lealferreira@gmail.com>
2020-02-20 21:56:35 -0300

    Finaliza sistema de usuários

--- projects/users/main.py
+++ projects/users/main.py
@@ -1,15 +1,26 @@
 from lib.interface import *
+from lib.file import *
+from time import sleep
 
+database = 'users-database.txt'
 opcoes = ['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema']
+
+if not fileExists(database):
+    makeFile(database)
+
 while True:
     resposta = menu(opcoes)
     if resposta == 1:
-        print('Opcao 1')
+        readFile(database)
     elif resposta == 2:
-        print('Opcao 2')
+        cabecalho('NOVO CADASTRO')
+        name = str(input('Nome: '))
+        age = leiaInt('Idade: ')
+        register(database, name, age)
     elif resposta == 3:
         print('Saindo do sistema...')
         break
     else:
         print('\033[31mERRO: Digite uma opção válida!\033[m')
+    sleep(2)
 
